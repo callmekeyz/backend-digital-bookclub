b70732f88fe6fd425bf49fb6e220281f5f14048a
Eunice Pereira <eunicepere333@gmail.com>
2021-01-01 15:31:16 -0600

    added frontend controllers

---
+++ controllers/frontend.js
@@ -0,0 +1,31 @@
+const homeController = (req, res) => {
+	res.render('home');
+};
+
+const aboutController = (req, res) => {
+	res.render('about');
+};
+
+const contactController = (req, res) => {
+	res.render('contact');
+};
+
+const exploreController = (req, res) => {
+	res.render('explore');
+};
+
+const loginController = (req, res) => {
+	res.render('login');
+};
+const signupController = (req, res) => {
+	res.render('signup');
+};
+
+module.exports = {
+	homeController,
+	aboutController,
+	contactController,
+	exploreController,
+	loginController,
+	signupController,
+};
