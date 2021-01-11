400855ad0bc383886ffb7b903e0159125f27dde9
Sarina Lyons <sarinalyons@gmail.com>
2021-01-11 13:00:58 -0500

    updated controllers

--- controllers/user.js
+++ controllers/user.js
@@ -1,44 +1,99 @@
-// const bcrypt = require('bcryptjs'); //npm install
-// const { User } = require('../models');
-const { layout } = require('../utils');
+const bcrypt = require("bcryptjs");
+const { layout } = require("../utils");
+const { Member } = require("../models");
 
 const newUser = (req, res) => {
-	res.render('create-acct', {
-		locals: {},
-		...layout,
-	});
+  res.render("home", {
+    locals: {
+      title: "Sign up",
+    },
+    ...layout,
+  });
 };
 
 const processNewUser = async (req, res) => {
-	// username + password
-	// bcrypt - salt and hash
+  const { username, password } = req.body;
+  console.log(username, password);
+  if (username == "" || password == "") {
+    // informs user of required info
+    console.log("username or password is blank", req.baseUrl);
+    res.redirect(`${req.baseUrl}/home`);
+  } else {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+    try {
+      const newUser = await Member.create({
+        username,
+        hash,
+      });
+      res.redirect(`${req.baseUrl}/create-acct`);
+    } catch (e) {
+      // e.name will be "SequelizeUniqueConstraintError"
+      console.log(e.name);
+      if (e.name === "SequelizeUniqueConstraintError") {
+        // We should tell the user that the username is taken
+        // and then redirect them
+        res.render("create-acct", {
+          locals: {
+            message: "That username is taken. Please try again.",
+          },
+        });
+      }
+      res.redirect(`${req.baseUrl}/home`);
+    }
+  }
 };
 
 const login = (req, res) => {
-	res.render('home', {
-		// login form is in html home page
-		locals: {},
-		...layout,
-	});
+  res.render("home", {
+    locals: {
+      title: "Log in",
+    },
+    ...layout,
+  });
 };
 
 const processLogin = async (req, res) => {
-	// get user by username
-	// check if password is correct
-	// save info about user in session
-	// once logged in, redirect to profile page
+  const { username, password } = req.body;
+  // find user by username
+  const user = await Member.findOne({
+    where: {
+      username,
+    },
+  });
+  if (user) {
+    console.log("valid user...checking password");
+    const isValid = bcrypt.compareSync(password, user.hash);
+    if (isValid) {
+      console.log("password is good!");
+      req.session.user = {
+        username,
+      };
+      req.session.save(() => {
+        res.redirect("/member-profile");
+      });
+    } else {
+      console.log("but password is wrong");
+      res.redirect(`${req.baseUrl}/home`);
+    }
+  } else {
+    console.log("not a valid user");
+    res.redirect(`${req.baseUrl}/home`);
+  }
 };
 
 const logout = (req, res) => {
-	// req.session.destroy
-	// redirect user to 'logout/goodbye' page
-	res.render('signout');
+  console.log("logging out...");
+  req.session.destroy(() => {
+    // deleting session:
+    res.redirect("/home");
+  });
 };
 
 module.exports = {
-	newUser,
-	processNewUser,
-	login,
-	processLogin,
-	logout,
+  newUser,
+  processNewUser,
+  login,
+  processLogin,
+  logout,
 };
