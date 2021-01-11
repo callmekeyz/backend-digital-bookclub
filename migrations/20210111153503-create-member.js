77ccca84e812b7442d75654f798764eb072285e9
Eunice Pereira <eunicepere333@gmail.com>
2021-01-11 10:15:26 -0600

    adds migrations

--- migrations/20210111153503-create-member.js
+++ migrations/20210111153503-create-member.js
@@ -1,14 +1,20 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Accounts', {
+    await queryInterface.createTable('Members', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      email: {
+      firstname: {
+        type: Sequelize.STRING
+      },
+      lastname: {
+        type: Sequelize.STRING
+      },
+      username: {
         type: Sequelize.STRING
       },
       password: {
@@ -25,6 +31,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Accounts');
+    await queryInterface.dropTable('Members');
   }
 };
